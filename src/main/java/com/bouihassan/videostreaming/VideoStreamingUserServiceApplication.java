7ad43c35921a33fdf0a614f9f1dad1e32b65b546
HassanBouighamdane <hassanbouighamdane2002@gmail.com>
2024-09-15 22:45:06 +0100

    first commit

---
+++ src/main/java/com/bouihassan/videostreaming/VideoStreamingUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bouihassan.videostreaming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoStreamingUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VideoStreamingUserServiceApplication.class, args);
+    }
+
+}
